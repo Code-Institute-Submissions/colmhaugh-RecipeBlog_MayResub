1d677873d214fb2380881ba8f8ef0de091a050e1
colmhaugh <colm.haugh@gmail.com>
2022-02-07 16:40:05 +0000

    Add recipe and comment data to models and add recipe model class to admin

--- recipies/admin.py
+++ recipies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'content']
+    list_filter = ('cource', 'status', 'created_on')
+    summernote_fields = ('description', 'ingredients', 'instructions')
+    list_display = ('title', 'slug', 'cource', 'status', 'created_on')
