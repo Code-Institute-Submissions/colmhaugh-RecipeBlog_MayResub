957584e2e58b158498a4b774ab744482cb54ff0f
colmhaugh <colm.haugh@gmail.com>
2022-02-09 12:42:26 +0000

    Add placeholeder for my recipies

--- recipies/views.py
+++ recipies/views.py
@@ -35,6 +35,12 @@ class DessertList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
 
+class MyRecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status = 1).order_by('-created_on') 
+    template_name = 'index.html'
+    paginate_by = 6
+
 
 class RecipeDetail(View):
 
